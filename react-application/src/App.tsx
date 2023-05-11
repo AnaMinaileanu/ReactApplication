e5f06c5cbf980c43ee86990c22560706b2c92b4f
AnaMinaileanu <minaileanuana@yahoo.com>
2023-05-11 14:26:47 +0300

    Set up packages and login, logout routes

---
+++ react-application/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Main } from './components/root/Main';
+import { Login } from './components/open-id-connect/Login';
+import { Logout } from './components/open-id-connect/Logout';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/logim" element={<Login/>}></Route>
+          <Route path="/logout" element={<Logout/>}></Route>
+          <Route path="/*" element={<Main/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ react-application/src/components/open-id-connect/Logout.tsx
@@ -0,0 +1,9 @@
+const Logout = () => {
+    return(
+        <>
+        
+        </>
+    );
+};
+
+export {Logout};
